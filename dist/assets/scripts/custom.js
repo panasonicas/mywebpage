a5bdf9027f61da50034432f4699d2f2062093acb
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-01-29 11:52:56 +0200

    Initial commit

---
+++ dist/assets/scripts/custom.js
@@ -0,0 +1,52 @@
+"use strict";
+
+tns({
+    container: '.my-slider',
+    items: 4,
+    controls: false,
+    controlsPosition: 'bottom',
+    controlsText: ['kaire','desne'],
+    nav: false,
+    autostopButton: false,
+    navPosition: 'bottom',
+    autoplay: true,
+    mouseDrag:true,
+});
+    
+//---------ALL SETTINGS FOR TINY SLIDER----------
+
+//   container: '.slider',
+//   mode: 'carousel', // or 'gallery'
+//   axis: 'horizontal', // or 'vertical'
+//   items: 1,
+//   gutter: 0,
+//   edgePadding: 0,
+//   fixedWidth: false,
+//   slideBy: 1,
+//   controls: true,
+//   controlsText: ['prev', 'next'],
+//   controlsContainer: false,
+//   nav: true,
+//   navContainer: false,
+//   navAsThumbnails: false,
+//   arrowKeys: false,
+//   speed: 300,
+//   autoplay: false,
+//   autoplayTimeout: 5000,
+//   autoplayDirection: 'forward',
+//   autoplayText: ['start', 'stop'],
+//   autoplayHoverPause: false,
+//   autoplayButton: false,
+//   autoplayButtonOutput: true,
+//   autoplayResetOnVisibility: true,
+//   loop: true,
+//   rewind: false,
+//   autoHeight: false,
+//   responsive: false,
+//   lazyload: false,
+//   touch: true,
+//   mouseDrag: false,
+//   swipeAngle: 15,
+//   nested: false,
+//   freezable: true,
+//   onInit: false
